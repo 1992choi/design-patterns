350cc943db5ad5a96a89b3d5a8f090c6a435c757
choi <younggeunn@naver.com>
2024-03-24 15:46:04 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 빌더 패턴 4부

---
+++ src/main/java/org/example/designpatterns/_01_creational/_04_builder/_03_java/StreamExample.java
@@ -0,0 +1,11 @@
+package org.example.designpatterns._01_creational._04_builder._03_java;
+
+import java.util.stream.Stream;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        Stream<String> names = Stream.<String>builder().add("spring").add("java").build();
+        names.forEach(System.out::println);
+    }
+}
