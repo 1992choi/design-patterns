c10ad9be180179714b2c77b427a441f997fc20e0
choi <younggeunn@naver.com>
2024-03-30 15:47:18 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 템플릿 메소드 패턴 4부

---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_22_template/_03_java/TemplateInJava.java
@@ -0,0 +1,11 @@
+package org.example.designpatterns._03_behavioral._22_template._03_java;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+    public static void main(String[] args) {
+        HttpServlet httpServlet;
+    }
+
+}
