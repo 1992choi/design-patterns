7adc73b23dc04d77d0251e339938c930b003388c
choi <younggeunn@naver.com>
2024-03-31 15:05:00 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 책임 연쇄 패턴 4부

---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_13_chain_of_responsibilities/_03_java/App.java
@@ -0,0 +1,15 @@
+package org.example.designpatterns._03_behavioral._13_chain_of_responsibilities._03_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
