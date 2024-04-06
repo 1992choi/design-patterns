a4501be64ee1d4f74b06f3380e010e94a0a2f9f1
choi <younggeunn@naver.com>
2024-04-06 13:56:09 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 메멘토 패턴 2부

---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_18_memento/_02_after/Client.java
@@ -0,0 +1,21 @@
+package org.example.designpatterns._03_behavioral._18_memento._02_after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.setBlueTeamScore(10);
+        game.setRedTeamScore(20);
+
+        GameSave save = game.save();
+
+        game.setBlueTeamScore(12);
+        game.setRedTeamScore(22);
+
+        game.restore(save);
+
+        System.out.println(game.getBlueTeamScore()); // 10
+        System.out.println(game.getRedTeamScore()); // 20
+    }
+
+}
