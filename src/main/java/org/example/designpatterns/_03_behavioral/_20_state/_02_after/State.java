bd69408949f59d094b12f7b5cac35a693842a664
choi <younggeunn@naver.com>
2024-04-07 20:15:21 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 상태 패턴 2부

---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_20_state/_02_after/State.java
@@ -0,0 +1,9 @@
+package org.example.designpatterns._03_behavioral._20_state._02_after;
+
+public interface State {
+
+    void addReview(String review, Student student);
+
+    void addStudent(Student student);
+
+}
