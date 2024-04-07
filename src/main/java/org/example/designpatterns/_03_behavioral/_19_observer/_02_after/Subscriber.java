e074e2039ba6f8029b58ed2af91cc3042504ac0a
choi <younggeunn@naver.com>
2024-04-07 17:28:53 +0900

    [인프런] 코딩으로 학습하는 GoF의 디자인 패턴 - 옵저버 패턴 2부

---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_19_observer/_02_after/Subscriber.java
@@ -0,0 +1,7 @@
+package org.example.designpatterns._03_behavioral._19_observer._02_after;
+
+public interface Subscriber {
+
+    void handleMessage(String message);
+
+}
---
+++ src/main/java/org/example/designpatterns/_03_behavioral/_19_observer/_02_after/User.java
@@ -0,0 +1,20 @@
+package org.example.designpatterns._03_behavioral._19_observer._02_after;
+
+public class User implements Subscriber {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void handleMessage(String message) {
+        System.out.println(message);
+    }
+
+}
